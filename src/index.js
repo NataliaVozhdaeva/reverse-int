2d729e99fca382c474f10348da62691c48e70f86
NataliaVozhdaeva <LomzNat@yandex.ru>
2023-01-25 00:09:03 +0200

    feat: add solving

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+  let newNumber = n;
+
+  if (n < 0) {
+    newNumber = n * -1;
+  }
+  let arr = Array.from(newNumber.toString());
+
+  return Number(arr.reverse().join(''));
+};
